facfe5e8483fa6e727bce6429ddd33fc7014e43e
maxgramf3@gmail.com <maxgramf3@gmail.com>
2020-08-03 23:14:43 +0900

    fix: clean scripts codes

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -3,9 +3,11 @@
 set -e
 
 
-#wget https://d3pxv6yz143wms.cloudfront.net/11.0.3.7.1/java-11-amazon-corretto-devel-11.0.3.7-1.x86_64.rpm
-#sudo yum localinstall java-11-amazon-corretto-devel-11.0.3.7-1.x86_64.rpm
-## /usr/bin/java
+#if [ ! -e "/usr/bin/java" ]; then
+#  wget https://d3pxv6yz143wms.cloudfront.net/11.0.3.7.1/java-11-amazon-corretto-devel-11.0.3.7-1.x86_64.rpm
+#  sudo yum localinstall java-11-amazon-corretto-devel-11.0.3.7-1.x86_64.rpm
+#fi
+
 #wget https://downloads.lightbend.com/scala/2.13.1/scala-2.13.1.tgz
 #tar xvzf ./scala-2.13.1.tgz
 #sudo mv scala-2.13.1 /usr/local/scala
